13dd732895ad470b4452f24c3c1f5504188e6c6b
jiyeonlab <jiyeonlab@gmail.com>
2021-10-27 19:52:37 +0900

    [Rename] login view controller

---
+++ RxSwift-SimpleLogin/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwift-SimpleLogin
+//
+//  Created by Jiyeon on 2021/10/27.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
