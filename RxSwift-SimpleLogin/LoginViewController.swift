fe7e0d5b1172d6b5104dcb7689f67a06b2406cea
jiyeonlab <jiyeonlab@gmail.com>
2021-10-27 19:55:50 +0900

    [Add] login button connect to class and set default style

--- RxSwift-SimpleLogin/LoginViewController.swift
+++ RxSwift-SimpleLogin/LoginViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            loginButton.tintColor = .white
+            loginButton.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+            loginButton.layer.cornerRadius = 15
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
